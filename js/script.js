6cd661b7f7a0c785e253acfab12c3b86c873be45
Raúl Ramírez <raul.ramirez@utp.ac.pa>
2021-05-25 22:34:27 -0500

    Se hicieron cambios de padding en busqueda

--- js/script.js
+++ js/script.js
@@ -111,7 +111,36 @@ collapseR.onclick = () =>{
 
 
 
+closeBtn.onclick = () =>{
+    overlay.classList.toggle
+    etElementsByTagName('body')[0].classList.toggle('lockScroll')
+}
 
+comment.onclick =() =>{
+    titulopost=document.getElementById("BusquedaTitle").textContent="Título del meme"
+    overlay.classList.toggle('hidden')
+    document.getElementsByTagName('body')[0].classList.toggle('lockScroll')
+}   
+
+query1.onclick =() =>{
+    titulopost=document.getElementById("BusquedaTitle").textContent=document.getElementById('textobusqueda').value
+    overlay.classList.toggle('hidden')
+    document.getElementsByTagName('body')[0].classList.toggle('lockScroll')
+}   
+
+
+closeBtn.onclick = () =>{
+    overlay.classList.toggle('hidden')
+    document.getElementsByTagName('body')[0].classList.toggle('lockScroll')
+}
+
+function myFunction(x) {
+    x.classList.toggle("fas");
+  }
+
+function Buscar(x) {
+    document.getElementById("section-title-h1").textContent= x
+}
 
 
 
